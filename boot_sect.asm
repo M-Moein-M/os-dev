a19f361c4df1efac1b2fda370856f1a525d3197e
moein <mohammadmoein.moradi@gmail.com>
2021-09-17 10:44:53 +0430

    print message usign BIOS routine

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,4 +1,18 @@
-loop:
+mov ah, 0x0e  ; scrolling teletype BIOS routine
+
+mov al, 'H'
+int 0x10
+mov al, 'E'
+int 0x10
+mov al, 'L'
+int 0x10
+mov al, 'L'
+int 0x10
+mov al, 'O'
+int 0x10
+
+
+loop:         ; infinite jump
 jmp loop
 
 times 510-($-$$) db 0
