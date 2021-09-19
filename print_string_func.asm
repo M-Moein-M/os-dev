cf22d4b2b00244bc0504f747539971ff1c0dc703
moein <mohammadmoein.moradi@gmail.com>
2021-09-19 13:04:35 +0430

    add print string function

---
+++ print_string_func.asm
@@ -0,0 +1,15 @@
+print_string:   ; print null-terminated string starting at value of "bx" register
+pusha
+
+while_loop:
+cmp word [bx], 0     ; return if we reach the null character
+je end_print_string
+mov ax, [bx]
+mov ah, 0x0e    ; BIOS tele-type output
+int 0x10        ; BIOS intrupt to print character on scrren
+inc bx
+jmp while_loop
+
+end_print_string:
+popa
+ret
