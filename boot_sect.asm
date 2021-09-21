93786dcdc480e46557dcd01dc38d9faef86240a6
moein <mohammadmoein.moradi@gmail.com>
2021-09-21 12:37:05 +0430

    fix position of including external file

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,10 +1,7 @@
 [ org 0x7c00 ]
 
 MSG:
-db 'Initiating boot...', 0
-
-%include "print_string_func.asm"
-
+db 'Initiating boot', 0
 
 mov bp, 0x8000
 mov sp, bp
@@ -13,6 +10,8 @@ mov bx, MSG
 call print_string
 
 jmp $         ; infinite jump
+
+%include "print_string_func.asm"
 
 times 510-($-$$) db 0
 
