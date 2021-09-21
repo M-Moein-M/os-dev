d805333d96bc4b374fea110588edb13589948bef
moein <mohammadmoein.moradi@gmail.com>
2021-09-21 12:36:39 +0430

    add size specifier to memory access

--- print_string_func.asm
+++ print_string_func.asm
@@ -2,9 +2,9 @@ print_string:   ; print null-terminated string starting at value of "bx" registe
 pusha
 
 while_loop:
-cmp word [bx], 0     ; return if we reach the null character
+cmp byte [bx], 0     ; return if we reach the null character
 je end_print_string
-mov ax, [bx]
+mov al, byte [bx]
 mov ah, 0x0e    ; BIOS tele-type output
 int 0x10        ; BIOS intrupt to print character on scrren
 inc bx
